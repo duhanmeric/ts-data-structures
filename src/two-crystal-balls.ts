9d3d80a12dafd8db2e74865ccd2a022f48c7f79f
duhanmeric <duhanmeric@gmail.com>
2023-12-03 21:38:05 +0300

    two crystal balls better explanation

--- src/two-crystal-balls.ts
+++ src/two-crystal-balls.ts
@@ -3,6 +3,7 @@ const twoCrystalBalls = (breaks: boolean[]) => {
   const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
   // array'deki ilk true'ya gelene kadar git, gelince break yapıp döngüden çık
+  // burda ilk top kırılıyor, 2. topu da atarsak kırılacağı noktayı buluyoruz
   let i = jmpAmount;
   for (; i < breaks.length; i += jmpAmount) {
     if (breaks[i]) {
@@ -13,6 +14,7 @@ const twoCrystalBalls = (breaks: boolean[]) => {
   // son kırılan yerden jmpAmount kadar geri sar
   i -= jmpAmount;
 
+  // son topun kırılmadan atılacağı noktayı bulma
   // artık sadece son checkpoint'ten jmpAmount kadar ileri gidecek (çünkü bu aralıkta kırılmış olmalı)
   // eğer kırılan noktaya geldiysen index'i return et
   for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i) {

7e22beecbcc3b203b90a79c2b45ce583f732d75e
duhanmeric <duhanmeric@gmail.com>
2023-12-27 22:04:17 +0300

    minheap

---
+++ src/structs/min-heap.ts
@@ -0,0 +1,81 @@
+export class MinHeap {
+  public length: number;
+  private data: number[];
+
+  constructor() {
+    this.length = 0;
+    this.data = [];
+  }
+
+  insert(value: number): void {
+    this.data[this.length] = value;
+    this.heapifyUp(this.length);
+    this.length++;
+  }
+
+  delete(): number {
+    if (this.length === 0) {
+      return -1;
+    }
+
+    const out = this.data[0];
+    this.data[0] = this.data[this.length - 1];
+    this.length--;
+
+    if (this.length > 0) {
+      this.heapifyDown(0);
+    }
+
+    return out;
+  }
+
+  private heapifyDown(i: number) {
+    const lChild = this.getLeftChild(i);
+    const rChild = this.getRightChild(i);
+
+    let smallest = i;
+
+    if (lChild < this.length && this.data[lChild] < this.data[smallest]) {
+      smallest = lChild;
+    }
+
+    if (rChild < this.length && this.data[rChild] < this.data[smallest]) {
+      smallest = rChild;
+    }
+
+    if (smallest !== i) {
+      [this.data[i], this.data[smallest]] = [this.data[smallest], this.data[i]];
+      this.heapifyDown(smallest);
+    }
+  }
+
+  private heapifyUp(i: number) {
+    if (i === 0) {
+      return;
+    }
+
+    const parentIndex = this.getParent(i); // 1
+    const parentValue = this.data[parentIndex]; // 5
+
+    const itemValue = this.data[i]; // 2
+
+    if (parentValue > itemValue) {
+      // 5 > 2 yes
+      this.data[i] = parentValue; // yeni eklenen data 5 oldu
+      this.data[parentIndex] = itemValue; // parent ise yeni eklenen data oldu
+      this.heapifyUp(parentIndex);
+    }
+  }
+
+  private getParent(i: number): number {
+    return Math.floor((i - 1) / 2);
+  }
+
+  private getLeftChild(i: number): number {
+    return i * 2 + 1;
+  }
+
+  private getRightChild(i: number): number {
+    return i * 2 + 2;
+  }
+}
